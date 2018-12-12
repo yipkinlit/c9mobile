cfcf016227cbf31f6314148cb81151cc43e96692
anreh <anurehka@gmail.com>
2018-12-12 13:29:18 +0800

    Initial commit

---
+++ app/src/main/java/com/example/taruc/myapplication/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.taruc.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    // The TextView where we will show results
+    //Key for saving the state of the TextView
+    private static final String TEXT_STATE = "currentText";
+
+    // The TextView where we will show results
+    private TextView mTextView = null;
+
+    /**
+     * Initializes the activity.
+     * @param savedInstanceState The current state data
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //  Initialize mTextView
+        mTextView = (TextView) findViewById(R.id.textView1);
+
+        // Restore TextView if there is a savedInstanceState
+        if(savedInstanceState!=null){
+            mTextView.setText(savedInstanceState.getString(TEXT_STATE));
+        }
+    }
+
+    /**`
+     * Handles the onCLick for the "Start Task" button. Launches the AsyncTask
+     * which performs work off of the UI thread.
+     *
+     * @param view The view (Button) that was clicked.
+     */
+    public void startTask (View view) {
+        // Put a message in the text view
+        mTextView.setText(R.string.napping);
+
+        // Start the AsyncTask.
+        // The AsyncTask has a callback that will update the text view.
+        new SimpleAsyncTask(mTextView).execute();
+    }
+
+
+    /**
+     * Saves the contents of the TextView to restore on configuration change.
+     * @param outState The bundle in which the state of the activity is saved
+     * when it is spontaneously destroyed.
+     */
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        // Save the state of the TextView
+        outState.putString(TEXT_STATE, mTextView.getText().toString());
+    }
+}
